6c91c380f71c23c649afcb87baaa218ee43d9799
joris-fhnw <joris.strassburg@fhnw.ch>
2024-06-24 12:54:44 +0200

    plt.show(block=True) hinzugefügt, plot freezed so nicht mehr

--- n2oReduction_equilibrium.py
+++ n2oReduction_equilibrium.py
@@ -26,7 +26,7 @@ x0_n2o = 10000e-6  # N2O in Air  [mol/mol]
 
 lam_min = 1.01
 lam_max = 2.5
-step = .1  # Auflösung
+step = .01  # Auflösung
 length = int(np.ceil((lam_max - lam_min) / step))  # Anzahl Berechnungsschritte
 lam_vek = np.arange(lam_min, lam_max, step)
 
@@ -69,6 +69,10 @@ for i, lam in enumerate(lam_vek):
 
 
 # Plot
+
+# fig = plt.Figure()
+# ax1 = plt.subplots()
+#
 fig, ax1 = plt.subplots()
 plt.plot(T_vek-T_norm, x_n2o * 1e6, label="N2O")
 plt.plot(T_vek-T_norm, x_no * 1e6, label="NO")
@@ -79,5 +83,5 @@ ax2 = ax1.twinx()
 ax2.plot(T_vek-T_norm, x_o2 * 100, color="grey", label="O2")
 ax2.set_ylabel("O2 [mol %]")
 fig.legend()
-plt.show()
+plt.show(block=True)
 
