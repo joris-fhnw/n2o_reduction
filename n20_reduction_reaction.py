42763672fec02b96849fd194f147f10c57af91e3
joris-fhnw <joris.strassburg@fhnw.ch>
2024-06-25 15:32:56 +0200

    Pluggflow reactor hinzugefügt

--- n20_reduction_reaction.py
+++ n20_reduction_reaction.py
@@ -20,64 +20,75 @@ def gas_lmin(syngas):
 
 # Combust Properties
 p = 10e5  # [Pa]
-T = 300  # [K]
+T_0 = 1500  # [K] inlet temperature
 T_norm = 273  # [K]
-x0_n2o = 10000e-6  # N2O in Air  [mol/mol]
+x0_n2o = 1000e-6  # N2O in Air  [mol/mol]
 
-lam_min = 1.01
-lam_max = 2.5
-step = .1  # resolution
-length = int(np.ceil((lam_max - lam_min) / step))  # number of steps
-lam_vek = np.arange(lam_min, lam_max, step)
+lam = 2
+
+length = 1.5e-6  # *approximate* PFR length [m]
+u_0 = .006  # inflow velocity [m/s]
+area = 1.e-4  # cross-sectional area [m**2]
+
+# Resolution: The PFR will be simulated by 'n_steps' time steps or by a chain
+# of 'n_steps' stirred reactors.
+n_steps = 2000
 
 # Cantera Objects
 air = ct.Solution("gri30.yaml")
 gas = ct.Solution("gri30.yaml")
-exhaust = ct.Solution("gri30.yaml")
+gasmix = ct.Solution("gri30.yaml")
 
-# Initialize
-x_n2o = np.zeros(length)
-x_n2 = np.zeros(length)
-x_no = np.zeros(length)
-x_no2 = np.zeros(length)
-T_vek = np.zeros(length)
-x_o2 = np.zeros(length)
+# Mixing of gas and combustion air
+gas.TPX = T_0, p, {"CH4": .45, "CO2": .55}
+air.TPX = T_0, p, {"N2": 0.79 * (1 - x0_n2o), "O2": 0.21 * (1 - x0_n2o), "N2O": x0_n2o}
+
+# Mixing of the gas and the combust air
+q1 = ct.Quantity(gas, mass=1)
+q2 = ct.Quantity(air, mass=gas_lmin(gas) * lam)
+q1.constant = q2.constant = 'HP'
+q3 = q1 + q2
+
+# calculating the exhaust
+X_gasmix = q3.X  # [mol/mol] Molanteile des Gas Luft gemisches
+gasmix.TPX = T_0, p, X_gasmix
+
+mass_flow_rate1 = u_0 * gasmix.density * area
+
+# create a new reactor
+r1 = ct.IdealGasConstPressureReactor(gasmix)
+# create a reactor network for performing time integration
+sim1 = ct.ReactorNet([r1])
+
+# approximate a time step to achieve a similar resolution as in the next method
+t_total = length / u_0
+dt = t_total / n_steps
+# define time, space, and other information vectors
+t1 = (np.arange(n_steps) + 1) * dt
+z1 = np.zeros_like(t1)
+u1 = np.zeros_like(t1)
+states1 = ct.SolutionArray(r1.thermo)
+for n1, t_i in enumerate(t1):
+    # perform time integration
+    sim1.advance(t_i)
+    # compute velocity and transform into space
+    u1[n1] = mass_flow_rate1 / area / r1.thermo.density
+    z1[n1] = z1[n1 - 1] + u1[n1] * dt
+    states1.append(r1.thermo.state)
+
+plt.figure()
+plt.plot(z1, states1.T)
+plt.xlabel('$z$ [m]')
+plt.ylabel('$T$ [K]')
+plt.legend(loc=0)
+plt.show(block=True)
 
 
-# Calculation of exhaust: equalize gas and air mixes with constant HP
-for i, lam in enumerate(lam_vek):
-    gas.TPX = T, p, {"CH4": .45, "CO2": .55}
-    air.TPX = T, p, {"N2": 0.79 * (1 - x0_n2o), "O2": 0.21 * (1 - x0_n2o), "N2O": x0_n2o}
-
-    # Mixing of the gas and the combust air
-    q1 = ct.Quantity(gas, mass=1)
-    q2 = ct.Quantity(air, mass=gas_lmin(gas) * lam)
-    q1.constant = q2.constant = 'HP'
-    q3 = q1 + q2
-
-    # calculating the exhaust
-    X_exhaust = q3.X  # [mol/mol] molfractions of gas and air mixture
-    exhaust.TPX = T, p, X_exhaust
-    exhaust.equilibrate("HP")  # equilibrium with constant pressure and enthalpie
-
-    x_n2o[i] = exhaust.X[exhaust.species_index("N2O")]
-    x_n2[i] = exhaust.X[exhaust.species_index("N2")]
-    x_no2[i] = air.X[exhaust.species_index("NO2")]
-    x_no[i] = exhaust.X[exhaust.species_index("NO")]
-    x_o2[i] = exhaust.X[exhaust.species_index("O2")]
-    T_vek[i] = exhaust.T
-
-
-# Plot
-fig, ax1 = plt.subplots()
-plt.plot(T_vek-T_norm, x_n2o * 1e6, label="N2O")
-plt.plot(T_vek-T_norm, x_no * 1e6, label="NO")
-plt.plot(T_vek-T_norm, x_no2 * 1e6, label="NO2")
-plt.xlabel("T_ad [°C]")
-plt.ylabel("[ppm]")
-ax2 = ax1.twinx()
-ax2.plot(T_vek-T_norm, x_o2 * 100, color="grey", label="O2")
-ax2.set_ylabel("O2 [mol %]")
-fig.legend()
-plt.show()
-
+plt.figure()
+plt.plot(t1, states1.X[:, gasmix.species_index('N2O')]*1e6, label='N2O')
+plt.plot(t1, states1.X[:, gasmix.species_index('NO')]*1e6, label='NO')
+plt.xlabel('$t$ [s]')
+plt.ylabel('$X$ [ppm]')
+plt.legend(loc=0)
+plt.show(block=True)
+# plt.savefig('pfr_XN2O_t.png')
